ad5022ff1337cf4189fba8cb02c742dbfca2abbb
Recox <5874806+RecoX@users.noreply.github.com>
2019-01-10 23:38:01 +1300

    Improved Image Details Page

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -42,32 +42,110 @@ class ImageDetails extends Component {
             <Query query={IMAGE_GET_BY_ID} variables={this._getQueryVariables()}>
                 {({ loading, error, data }) => {
                     console.log(error)
-                    if (loading) return <div>Fetching</div>;
+                    if (loading) return <div>Cargando</div>;
                     if (error) return <div>Error</div>;
 
                     return (
                         <Fragment>
-                            <div className="flex mt2 items-start">
-                                <div className="ml1">
-                                    <div>
-                                        {data.image.name}
-                                    </div>
-                                    <img alt={data.image.name} width="200px" src={data.image.url}></img>
-                                    <div>
-                                        {data.image.description}
-                                    </div>
-                                    <div className="f6 lh-copy gray">
-                                        Creado en server: {data.image.origin}
-                                    </div>
-                                    <div className="f6 lh-copy gray">
-                                        {data.image.votes.length} votes |
-                                        Subido por {data.image.uploadedBy
-                                        ? data.image.uploadedBy.name
-                                        : 'Unknown'}{' - '}
-                                        {timeDifferenceForDate(data.image.createdAt)}
+                            <div className="container">
+                                <div className="nk-gap-4"></div>
+
+                                <div className="nk-store-product">
+                                    <div className="row xl-gap vertical-gap align-items-center">
+                                        <div className="col-md-5">
+                                            <div className="nk-carousel-3" data-size="1">
+                                                <div className="nk-carousel-inner nk-popup-gallery">
+
+                                                    <div>
+                                                        <div>
+                                                            <img alt={data.image.name} width="240px"
+                                                                 src={data.image.url}/>
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="nk-gap-1"></div>
+
+                                                    <div>
+                                                        <div>
+                                                            <img alt={data.image.name} width="240px"
+                                                                 src={data.image.url}/>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <div className="col-md-7">
+                                            <h2 className="nk-product-title">{data.image.name}</h2>
+
+                                            <div className="nk-product-description">
+                                                <p>{data.image.description}</p>
+                                            </div>
+
+                                            <aside className="nk-sidebar nk-sidebar-right">
+                                                <div className="nk-gap-5 d-none + d-lg-block"></div>
+                                                <div className="nk-gap d-lg-none"></div>
+                                                <table>
+                                                    <tbody>
+                                                    <tr>
+                                                        <td><strong>Subido hace:</strong> &nbsp;&nbsp;&nbsp;</td>
+                                                        <td>{timeDifferenceForDate(data.image.createdAt)}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><strong>Categoria:</strong> &nbsp;&nbsp;&nbsp;</td>
+                                                        <td><a href="#">News</a></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><strong>Creado en:</strong> &nbsp;&nbsp;&nbsp;</td>
+                                                        <td><span>{data.image.origin}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><strong>Subido por:</strong> &nbsp;&nbsp;&nbsp;</td>
+
+                                                        <td><span>{data.image.uploadedBy
+                                                            ? data.image.uploadedBy.name
+                                                            : 'Unknown'}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><strong>Favoritos:</strong> &nbsp;&nbsp;</td>
+                                                        <td>
+                                                            <span className="nk-action-heart">
+                                                                <span className="num">{data.image.votes.length}</span>
+                                                                <span className="like-icon ion-android-favorite-outline"></span>
+                                                                <span className="liked-icon ion-android-favorite"></span>
+                                                            </span>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <div className="nk-gap-5 d-lg-none"></div>
+                                            </aside>
+
+                                            <button className="nk-btn nk-btn-x2 link-effect-4">Descargar</button>
+                                        </div>
+
+
+                                        <div className="tab-content">
+                                            <div role="tabpanel" className="tab-pane fade show active"
+                                                 id="tab-description">
+                                                <div className="nk-gap-3"></div>
+                                                <div className="nk-box-3 bg-dark-1">
+                                                    <p>And gathering. Form for, gathering, female you'll blessed appear
+                                                        day us cattle hath be moving face he Whales fruitful is spirit
+                                                        Beginning. Abundantly good living Thing isn't stars saw over and
+                                                        earth dry rule herb bring image night, fowl their, third set saw
+                                                        for. Green a also upon life stars, green and darkness
+                                                        greater.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
+
                         </Fragment>
                     )
                 }}
